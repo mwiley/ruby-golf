2b5395947b80796f805ada66204e9a7c2614d93f
Mason Wiley <masonwiley92@gmail.com>
2016-04-24 17:27:07 -0700

    update sandbox ip

--- app/services/sandbox_service.rb
+++ app/services/sandbox_service.rb
@@ -1,6 +1,6 @@
 class SandboxService
   def self.query(problem_name, code)
-    uri = URI('http://localhost:4567/eval/' + problem_name)
+    uri = URI('http://192.168.2.7:4567/eval/' + problem_name)
     res = Net::HTTP.post_form(uri, code: code)
     JSON.parse(res.body)
   end
