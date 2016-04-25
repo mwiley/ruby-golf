49c48b0648d6d9a05d9061ec2df689ae6f16d2e3
Mason Wiley <masonwiley92@gmail.com>
2016-04-24 20:47:40 -0700

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,19 +9,30 @@
 challenges = [
  {
    title: 'hello',
-   description: 'transform the contents of the string :hello from "hello world" to "hello universe"'
+   description: 'transform the contents of the string :hello from "hello world" to "hello universe"',
+   start: "hello = 'hello world'", end: "hello = 'hello universe'",
   },
   {
     title: 'sort',
-    description: 'The string :nums contains integers seperated by spaces. Transform :nums into a string with these integers sorted.'
+    description: 'The string :nums contains integers seperated by spaces. Transform :nums into' +
+    ' a string with these integers sorted.',
+    start: "hello = 'hello world'", end: "nums = '134 12 53 1 269 23 1 57'"
   },
   {
     title: 'fizzbuzz',
-    description: 'Given an integer, define a function fizzbuzz that returns “Fizz” if it is a multiple of 3, “Buzz” if it is a multiple of 5 and “FizzBuzz” if it is a multiple of 15. If the number is not a multiple of 3 or 5 then the number is returned as a string.'
+    description: 'Given an integer, define a function fizzbuzz that returns “Fizz”' +
+    ' if it is a multiple of 3, "" “Buzz” if it is a multiple of 5 and “FizzBuzz” if' +
+    ' it is a multiple of 15. If the number is not a multiple of 3 or 5 then the' +
+    ' number is returned as a string.',
+    example: "fizzbuzz(3) == 'Fizz'\nfizzbuzz(10) == 'Buzz'\n" +
+    "fizzbuzz(45) == 'Fizzbuzz'\nfizzbuzz(31) == '31'"
   },
   {
     title: 'fizzbuzz2',
-    description: "For integers 1-100, output 'Fizz' if the number is divisible by 3, 'Buzz' if it's divisible by 5, and 15 if it's divisible by both. If it's not divisible by any of these, output the number. All outputs should be stored in a string :fizzbuzz and seperated by a single space."
+    description: "For integers 1-100, output 'Fizz' if the number is divisible by 3,"+
+    " 'Buzz' if it's divisible by 5, and 15 if it's divisible by both. If it's not divisible" +
+    " by any of these, output the number. All outputs should be stored in a string" +
+    " :fizzbuzz and seperated by a single space."
   }
 ]
 
