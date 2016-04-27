c51e95710c2082ee06485d6c80bfc8ba485b42b6
Mason Wiley <masonwiley92@gmail.com>
2016-04-27 14:38:46 -0700

    configure for production

--- app/services/sandbox_service.rb
+++ app/services/sandbox_service.rb
@@ -1,6 +1,12 @@
 class SandboxService
+  if Rails.env.production?
+    endpoint_url = ENV['SANDBOX_URL']
+  else
+    endpoint_url = 'http://192.168.2.7:4567/eval/'
+  end
+
   def self.query(problem_name, code)
-    uri = URI('http://192.168.2.7:4567/eval/' + problem_name)
+    uri = URI(endpoint_url + problem_name)
     res = Net::HTTP.post_form(uri, code: code)
     JSON.parse(res.body)
   end
