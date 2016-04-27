2919a36c7fcf71adfc59ceef66b7a3fb0c777640
Mason Wiley <masonwiley92@gmail.com>
2016-04-27 14:46:14 -0700

    fix endpoint url config

--- app/services/sandbox_service.rb
+++ app/services/sandbox_service.rb
@@ -1,11 +1,11 @@
 class SandboxService
-  if Rails.env.production?
-    endpoint_url = ENV['SANDBOX_URL']
-  else
-    endpoint_url = 'http://192.168.2.7:4567/eval/'
-  end
-
   def self.query(problem_name, code)
+    if Rails.env.production?
+      endpoint_url = ENV['SANDBOX_URL']
+    else
+      endpoint_url = 'http://192.168.2.7:4567/eval/'
+    end
+    
     uri = URI(endpoint_url + problem_name)
     res = Net::HTTP.post_form(uri, code: code)
     JSON.parse(res.body)
