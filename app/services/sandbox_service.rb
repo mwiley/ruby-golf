e8e821221c1e32c6f7a37ee47919dcd097a4aba3
Mason Wiley <masonwiley92@gmail.com>
2016-04-28 14:28:11 -0700

    update development sandbox endpoint url

--- app/services/sandbox_service.rb
+++ app/services/sandbox_service.rb
@@ -3,9 +3,9 @@ class SandboxService
     if Rails.env.production?
       endpoint_url = ENV['SANDBOX_URL']
     else
-      endpoint_url = 'http://192.168.2.7:4567/eval/'
+      endpoint_url = 'http://192.168.2.6:4567/eval/'
     end
-    
+
     uri = URI(endpoint_url + problem_name)
     res = Net::HTTP.post_form(uri, code: code)
     JSON.parse(res.body)
