3f72e7bdcc5ac7f609835f2ccf8c86866ed90452
Mason Wiley <masonwiley92@gmail.com>
2016-04-29 16:43:12 -0700

    add about page

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   devise_for :users
 
   root 'challenges#index'
+  get 'about/' => 'pages#about', as: 'about_page'
+
   resources :challenges do
     resources :submissions
   end
