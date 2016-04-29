9a1fc751f4f85a4f22fb8976a03b8127c09e115e
Mason Wiley <masonwiley92@gmail.com>
2016-04-28 19:26:13 -0700

    add summation of primes problem

--- db/seeds.rb
+++ db/seeds.rb
@@ -51,6 +51,12 @@ challenges = [
     description: 'By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that' +
     ' the 6th prime is 13. What is the 10001st prime number?',
     example: 'prime = #your answer here'
+  },
+  {
+    title: 'summation_of_primes',
+    description: 'The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17. ' +
+    'Find the sum of all the primes below two million.',
+    example: 'sum = #your answer here',
   }
 ]
 
