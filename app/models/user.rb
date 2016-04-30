4758f8ceb9ad226b4f2ca81c1f6301ff05a6c2ac
Mason Wiley <masonwiley92@gmail.com>
2016-04-29 18:32:50 -0700

    validate uniqueness of usernames

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,5 @@ class User < ActiveRecord::Base
   attr_accessor :current_password
 
   has_many :submissions, dependent: :destroy
-  validates :username, presence: true, length: { minimum: 2, maximum: 20 }
+  validates :username, presence: true, uniqueness: true, length: { minimum: 2, maximum: 20 }
 end
